7a585c5ff7a64f13f6b3a4c493f911f6512e5413
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-04-08 17:48:35 +0200

    continued implementing styling

--- src/components/Forms/DepositForm.jsx
+++ src/components/Forms/DepositForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import "./deposit-form.scss";
+import "./forms.scss";
 import * as Yup from "yup";
 
 const DepositForm = (props) => {
