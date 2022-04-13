51a19982260b5a86d3fc3a32a70706335c0c56fe
Eric Nordelo <eric.nordelo39@gmail.com>
2022-04-13 19:10:43 +0200

    feat: add web3modal config

--- src/pages/DepositPage/DepositPage.jsx
+++ src/pages/DepositPage/DepositPage.jsx
@@ -1,79 +1,129 @@
-import { useRef, useState, useEffect } from "react";
-import "./deposit-page.scss";
-import DepositForm from "../../components/Forms/DepositForm";
-import { connectWallet, getCurrentWalletConnected } from "../../utils/interact";
-import { ReactComponent as Logo } from "../../static/logo.svg";
-import gsap from "gsap";
-import Tooltip from "../../components/Tooltip/Tooltip";
+import Web3 from 'web3';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+
+import DepositForm from '../../components/Forms/DepositForm';
+import Tooltip from '../../components/Tooltip/Tooltip';
+import gsap from 'gsap';
+
+import { useRef, useState, useEffect } from 'react';
+import { ReactComponent as Logo } from '../../static/logo.svg';
+
+import './deposit-page.scss';
 
 const DepositPage = () => {
-  const [walletAddress, setWallet] = useState("");
-  const [status, setStatus] = useState("");
+  const INITIAL_STATE = {
+    walletAddress: '',
+    status: '',
+    web3: null,
+    provider: null,
+    connected: false,
+    chainId: 1,
+    networkId: 1,
+  };
+
+  const [walletAddress, setWallet] = useState(INITIAL_STATE.walletAddress);
+  const [status, setStatus] = useState(INITIAL_STATE.status);
+  const [web3, setWeb3] = useState(INITIAL_STATE.web3);
+  const [provider, setProvider] = useState(INITIAL_STATE.provider);
+  const [connected, setConnected] = useState(INITIAL_STATE.connected);
+  const [chainId, setChainId] = useState(INITIAL_STATE.chainId);
+  const [networkId, setNetworkId] = useState(INITIAL_STATE.networkId);
+
+  const infuraId = 'process.env.REACT_APP_INFURA_ID';
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        infuraId,
+      },
+    },
+  };
+
+  // configure the web3modal
+  const web3Modal = new Web3Modal({
+    cacheProvider: false,
+    disableInjectedProvider: false,
+    providerOptions,
+  });
 
   const el = useRef();
   const q = gsap.utils.selector(el);
 
   const fadeIn = (selector) => {
     const t1 = gsap.timeline();
-    t1.to(q(`${selector}`), { display: "flex", opacity: 1 }).to(
-      q(`${selector}`),
-      {
-        opacity: 0,
-        display: "none",
-        delay: 4,
-      }
-    );
+    t1.to(q(`${selector}`), { display: 'flex', opacity: 1 }).to(q(`${selector}`), {
+      opacity: 0,
+      display: 'none',
+      delay: 4,
+    });
 
     return () => {
       t1.kill();
     };
   };
 
-  console.log(status);
+  const onConnect = async () => {
+    if (connected) return;
 
-  const addWalletListener = async () => {
-    if (window.ethereum) {
-      handleEthereum();
-    } else {
-      window.addEventListener('ethereum#initialized', handleEthereum, {
-        once: true,
-      });
-    
-      // If the event is not dispatched by the end of the timeout,
-      // the user probably doesn't have MetaMask installed.
-      setTimeout(handleEthereum, 3000); // 3 seconds
-    }
+    const provider = await web3Modal.connect();
+
+    await subscribeProvider(provider);
+
+    const web3 = new Web3(provider);
+
+    // await provider.enable();
+
+    const accounts = await web3.eth.getAccounts();
+
+    const address = accounts[0];
+
+    const networkId = await web3.eth.net.getId();
+
+    const chainId = await web3.eth.getChainId();
+
+    // update state
+    setConnected(true);
+    setWeb3(web3);
+    setWallet(address);
   };
 
-  function handleEthereum() {
-    const { ethereum } = window;
-    if (ethereum  && ethereum.isMetaMask) {
-      ethereum.on("accountsChanged", (accounts) => {});
-    } else {
-      setStatus(
-        "🦊 You must install Metamask, a virtual Ethereum wallet, in your browser."
-      );
+  const resetApp = async () => {
+    if (web3 && web3.currentProvider && web3.currentProvider.close) {
+      await web3.currentProvider.close();
     }
+    await web3Modal.clearCachedProvider();
+
+    setWallet(INITIAL_STATE.walletAddress);
+    setStatus(INITIAL_STATE.status);
+    setWeb3(INITIAL_STATE.web3);
+    setProvider(INITIAL_STATE.provider);
+    setConnected(INITIAL_STATE.connected);
+    setChainId(INITIAL_STATE.chainId);
+    setNetworkId(INITIAL_STATE.networkId);
   };
-  }
 
-  useEffect(() => {
-    async function fetchData() {
-      const getWallet = await getCurrentWalletConnected();
-      setWallet(getWallet.address);
+  const subscribeProvider = async (provider) => {
+    if (!provider.on) {
+      return;
     }
-    fetchData();
-    addWalletListener();
-  }, []);
-
-  const connectWalletPressed = async () => {
-    const walletResponse = await connectWallet();
-    setWallet(walletResponse.address);
-
-    if (status.length !== 0) {
-      fadeIn(".deposit__tooltip--right");
-    }
+    provider.on('close', () => resetApp());
+    provider.on('accountsChanged', async (accounts) => {
+      setWallet(accounts[0]);
+    });
+    provider.on('chainChanged', async (chainId) => {
+      const networkId = await web3.eth.net.getId();
+      setChainId(chainId);
+      setNetworkId(networkId);
+    });
+    provider.on('networkChanged', async (networkId) => {
+      const chainId = await web3.eth.getChainId();
+      setChainId(chainId);
+      setNetworkId(networkId);
+    });
   };
+
+  useEffect(() => {}, []);
 
   const onMintPressed = async () => {
     //TODO: implement
@@ -87,42 +137,31 @@ const DepositPage = () => {
     <div className="deposit u__center" ref={el}>
       <div className="deposit__hero u__center">
         <Logo className="deposit__logo" />
-        <h2 className=" heading heading__secondary deposit__heading">
-          Item Deposit
-        </h2>
+        <h2 className=" heading heading__secondary deposit__heading">Item Deposit</h2>
 
         <img className="deposit__hero-image" alt=""></img>
         <div className="deposit__hero-content u__center">
+          <p>Tell us about yourself and the collectible you would like to vault.</p>
           <p>
-            Tell us about yourself and the collectible you would like to vault.
-          </p>
-          <p>
-            A personal concierge will reach out to walk you through the vaulting
-            and NFT-minting of your collectible.
+            A personal concierge will reach out to walk you through the vaulting and NFT-minting of
+            your collectible.
           </p>
         </div>
         <div className="deposit__wallet-buttons u__relative flex__aic">
           <Tooltip
             className="deposit__tooltip--left"
-            message={
-              "Please fill out the form below and your concierge will help you create one."
-            }
-            direction={"right"}
+            message={'Please fill out the form below and your concierge will help you create one.'}
+            direction={'right'}
           />
           <div className="btn__outline--outer gradient__orange">
             {walletAddress ? (
-              <button
-                className="btn btn__outline--inner deposit__btn"
-                onClick={() => {
-                  setWallet("");
-                }}
-              >
+              <button className="btn btn__outline--inner deposit__btn" onClick={resetApp}>
                 Disconnect Wallet
               </button>
             ) : (
               <button
                 className="btn btn__outline--inner deposit__btn"
-                onClick={() => fadeIn(".deposit__tooltip--left")}
+                onClick={() => fadeIn('.deposit__tooltip--left')}
               >
                 I don't have a wallet
               </button>
@@ -133,38 +172,26 @@ const DepositPage = () => {
             <button
               className="btn btn__outline--inner deposit__btn u__flex flex__jcsa flex__aic"
               id="walletButton"
-              onClick={connectWalletPressed}
+              onClick={onConnect}
             >
               {walletAddress.length > 0 ? (
                 <>
                   <span>
                     {String(walletAddress).substring(0, 6) +
-                      "..." +
+                      '...' +
                       String(walletAddress).substring(38)}
                   </span>
-                  <div
-                    className={`status-dot ${
-                      walletAddress ? "bg__green" : "bg__grey"
-                    }`}
-                  ></div>
+                  <div className={`status-dot ${walletAddress ? 'bg__green' : 'bg__grey'}`}></div>
                 </>
               ) : (
                 <>
-                  <div>Connect Wallet</div>{" "}
-                  <div
-                    className={`status-dot ${
-                      walletAddress ? "bg__green" : "bg__grey"
-                    }`}
-                  ></div>
+                  <div>Connect Wallet</div>{' '}
+                  <div className={`status-dot ${walletAddress ? 'bg__green' : 'bg__grey'}`}></div>
                 </>
               )}
             </button>
           </div>
-          <Tooltip
-            className="deposit__tooltip--right"
-            message={status}
-            direction={"left"}
-          />
+          <Tooltip className="deposit__tooltip--right" message={status} direction={'left'} />
         </div>
       </div>
       <div className="deposit__form u__flex flex__jcc">
---
+++ src/utils/chains.js
@@ -0,0 +1,20 @@
+const supportedChains = [
+  {
+    name: 'Polygon',
+    short_name: 'polygon',
+    chain: 'polygon',
+    network: 'polygon',
+    chain_id: 137,
+    network_id: 137,
+    rpc_url: 'https://polygon-rpc.com',
+    native_currency: {
+      symbol: 'MATIC',
+      name: 'MATIC',
+      decimals: '18',
+      contractAddress: '',
+      balance: '',
+    },
+  },
+];
+
+export default supportedChains;
