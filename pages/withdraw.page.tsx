88cc1cfec3618172ef842f6e533e19a6e61314a3
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-04-25 23:30:57 +0200

    created basic withdraw form

---
+++ pages/withdraw.page.tsx
@@ -0,0 +1,75 @@
+import styles from './deposit.module.scss';
+
+import { useRef } from 'react';
+import { useWeb3Context } from '../src/libs/hooks/useWeb3Context';
+import { ConnectWalletButton } from '../src/components/WalletConnection/ConnectWalletButton';
+
+import Logo from '../public/logo.svg';
+
+import WithdrawForm from '../src/components/Forms/WithdrawForm';
+import Tooltip from '../src/components/Tooltip/Tooltip';
+import gsap from 'gsap';
+
+export default function DepositPage() {
+  const { currentAccount, loading: web3Loading, disconnectWallet } = useWeb3Context();
+
+  const el = useRef();
+  const q = gsap.utils.selector(el);
+
+  const fadeIn = (selector) => {
+    const t1 = gsap.timeline();
+    t1.to(q(`${selector}`), { display: 'flex', opacity: 1 }).to(q(`${selector}`), {
+      opacity: 0,
+      display: 'none',
+      delay: 4,
+    });
+
+    return () => {
+      t1.kill();
+    };
+  };
+
+  return (
+    <div className={`${styles.deposit} u__center`} ref={el}>
+      <div className={`${styles.deposit__hero} u__center`}>
+        <img src={Logo.src} className={`heading ${styles.deposit__logo}`} />
+
+        <h2 className={`heading heading__secondary ${styles.deposit__heading}`}>Item Withdraw</h2>
+
+        <img className={`${styles['deposit__hero-image']}`} alt=""></img>
+        <div className={`${styles['deposit__hero-content']} u__center`}>
+          <p>Tell us about yourself and the collectible you would like to withdraw. If you originally vaulted this collectible, check the box below and tell us your wallet address and NFT ID to continue.</p>
+        </div>
+        <div className={`${styles['deposit__wallet-buttons']} u__relative flex__aic`}>
+          <Tooltip
+            className={`${styles['deposit__tooltip--left']}`}
+            message={'Please fill out the form below and your concierge will help you create one.'}
+            direction={'right'}
+          />
+          <div className={`btn__outline--outer bg__grey`}>
+            {currentAccount ? (
+              <button
+                className={`btn__outline--inner btn ${styles.deposit__btn}`}
+                onClick={disconnectWallet}
+              >
+                Disconnect Wallet
+              </button>
+            ) : (
+              <button
+                className={`btn__outline--inner btn btn__inactive ${styles.deposit__btn}`}
+                onClick={() => fadeIn(`.${styles['deposit__tooltip--left']}`)}
+              >
+                Disconnect Wallet
+              </button>
+            )}
+          </div>
+
+          <ConnectWalletButton />
+        </div>
+      </div>
+      <div className={`${styles['deposit__form']} u__flex flex__jcc`}>
+        <WithdrawForm additionalData={{ currentAccount }} />
+      </div>
+    </div>
+  );
+}
--- src/components/Forms/WithdrawForm.jsx
+++ src/components/Forms/WithdrawForm.jsx
@@ -1,7 +1,326 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { API } from 'aws-amplify';
+import Link from 'next/link';
+import styles from './forms.module.scss';
+import * as Yup from 'yup';
+import { Button } from '@aws-amplify/ui-react';
+import { useRouter } from 'next/router';
 
-const OuttakeForm = () => {
-  return <div>OuttakeForm</div>;
+const WithdrawForm = (props) => {
+  const router = useRouter();
+  const d = new Date();
+  const daysArray = [...Array(32).keys()].slice(1);
+  const monthsArray = [...Array(13).keys()].slice(1);
+  const yearsArray = [...Array(d.getFullYear()).keys()].slice(1900);
+  const statesArray = [
+    'AL',
+    'AK',
+    'AS',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FM',
+    'FL',
+    'GA',
+    'GU',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MH',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'MP',
+    'OH',
+    'OK',
+    'OR',
+    'PW',
+    'PA',
+    'PR',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VI',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
+  ];
+  const walletAddress = props.additionalData;
+
+  const apiName = 'vulcanAPI';
+  const path = '/deposit';
+  const [success, setSuccess] = useState(false);
+  const [serverMessage, setServerMessage] = useState('');
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        month: 'MM',
+        day: 'DD',
+        year: 'YYYY',
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        zip: '',
+        itemName: '',
+        itemDesc: '',
+      }}
+      validationSchema={Yup.object({
+        firstName: Yup.string().required('Required'),
+        lastName: Yup.string().required('Required'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+        month: Yup.string()
+          .test('is-month', 'Required', (value) => value !== 'MM')
+          .required('Required'),
+        day: Yup.string()
+          .test('is-day', 'Required', (value) => value !== 'DD')
+          .required('Required'),
+        year: Yup.string()
+          .test('is-year', 'Required', (value) => value !== 'YYYY')
+          .required('Required'),
+        address1: Yup.string().required('Required'),
+        address2: '',
+        city: Yup.string().required('Required'),
+        state: Yup.string()
+          .test('is-state', 'Required', (value) => value !== 'state')
+          .required('Required'),
+        zip: Yup.number().required('Required'),
+        itemName: Yup.string().required('Required'),
+        itemDesc: Yup.string().required('Required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        values.walletAddress = props.additionalData.currentAccount;
+
+        const myInit = {
+          body: {
+            dateOfBirth: `${values.year}-${values.month}-${values.day}`,
+            walletAddress: values.walletAddress,
+            ...values,
+          },
+        };
+
+        API.put(apiName, path, myInit)
+          .then((response) => {
+            console.log(response);
+            console.log('walletaddress:', walletAddress);
+            setSuccess(true);
+            setServerMessage(response.message);
+            router.push('/success');
+            //
+
+            setSubmitting(false);
+          })
+          .catch((error) => {
+            console.log(error);
+            setServerMessage(error.message);
+            setSubmitting(false);
+          });
+        // const createDateOfBirth = (values) => {
+        //   const dateOfBirth = `${values.year}-${values.month}-${values.day}`;
+        //   return dateOfBirth;
+        // };
+
+        // const myInit = {
+        //   body: {
+        //     ...values,
+        //     dateOfBirth: createDateOfBirth(values),
+        //     walletAddress: values.walletAddress,
+        //   },
+        // };
+
+        // API.put(apiName, path, myInit)
+        //   .then((response) => {
+        //     console.log(response);
+        //     setSubmitting(false);
+        //   })
+        //   .catch((error) => {
+        //     console.log(error.response);
+        //     setSubmitting(false);
+        //   });
+
+        // setTimeout(() => {
+        //   alert(JSON.stringify(values, null, 2));
+        //   setSubmitting(false);
+        // }, 400);
+      }}
+    >
+      <Form className={`${styles.form} u__left`}>
+        <div className={`${styles.form__row}`}>
+          <Field
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+            className={`${styles.form__field}`}
+          />
+          {/* <ErrorMessage name="firstName" /> */}
+
+          <Field
+            name="lastName"
+            type="text"
+            placeholder="Last Name"
+            className={`${styles.form__field}`}
+          />
+          {/* <ErrorMessage name="lastName" /> */}
+
+          <Field
+            name="email"
+            type="email"
+            placeholder="Email Address"
+            className={`${styles.form__field}`}
+          />
+          {/* <ErrorMessage name="email" /> */}
+        </div>
+
+        <div className={`${styles.form__row}`}>
+          <div className={`${styles.form__col}`}>
+            <label htmlFor="month" className="mb16 u__block">
+              Birthday
+            </label>
+            <div className={`${styles['form__select-group']}`}>
+              <div className={`${styles['form__select-wrapper']} u__flex flex__aic`}>
+                <div className={`${styles['form__select-arrow']}`}></div>
+                <Field
+                  name="month"
+                  as="select"
+                  className={`${styles.form__select} ${styles.form__field}`}
+                >
+                  <option value="MM">MM</option>
+                  {monthsArray.map((i) => {
+                    const month = i.toString().padStart(2, '0');
+                    return <option value={`${month}`}>{month}</option>;
+                  })}
+                </Field>
+                {/* <ErrorMessage name="month" /> */}
+              </div>
+
+              <div className={`${styles['form__select-wrapper']} u__flex flex__aic`}>
+                <div className={`${styles['form__select-arrow']}`}></div>
+                <Field
+                  name="day"
+                  as="select"
+                  className={`${styles.form__select} ${styles.form__field}`}
+                >
+                  <option value="DD">DD</option>
+                  {daysArray.map((i) => {
+                    const day = i.toString().padStart(2, '0');
+                    return <option value={`${day}`}>{day}</option>;
+                  })}
+                </Field>
+                {/* <ErrorMessage name="day" /> */}
+              </div>
+
+              <div className={`${styles['form__select-wrapper']} u__flex flex__aic`}>
+                <div className={`${styles['form__select-arrow']}`}></div>
+                <Field
+                  name="year"
+                  as="select"
+                  className={`${styles.form__select} ${styles.form__field}`}
+                >
+                  <option value="YYYY">YYYY</option>
+                  {yearsArray.map((i) => {
+                    return <option value={`${i}`}>{i}</option>;
+                  })}
+                </Field>
+                {/* <ErrorMessage name="year" /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={`${styles.form__row}`}>
+          <Field
+            name="address1"
+            type="text"
+            placeholder="Address 1"
+            className={`${styles.form__field}`}
+          />
+          {/* <ErrorMessage name="address1" /> */}
+
+          <Field
+            name="address2"
+            type="text"
+            placeholder="Address 2"
+            className={`${styles.form__field}`}
+          />
+          {/* <ErrorMessage name="address2" /> */}
+
+          <Field name="city" type="text" placeholder="City" className={`${styles.form__field}`} />
+          {/* <ErrorMessage name="city" /> */}
+
+          <div className={`${styles['form__state-zip']}`}>
+            <div className={`${styles['form__select-wrapper']} u__flex flex__aic`}>
+              <div className={`${styles['form__select-arrow']}`}></div>
+              <Field
+                name="state"
+                as="select"
+                className={`${styles.form__select} ${styles.form__field}`}
+              >
+                <option value="state">State</option>
+                {statesArray.map((i) => {
+                  return <option value={`${i}`}>{i}</option>;
+                })}
+              </Field>
+              {/* <ErrorMessage name="state" /> */}
+            </div>
+
+            <Field name="zip" type="text" placeholder="Zip" className={`${styles.form__field}`} />
+            {/* <ErrorMessage name="zip" /> */}
+          </div>
+        </div>
+
+        <div className={`${styles.form__row}`}>
+          <Field
+            name="itemName"
+            type="text"
+            placeholder="NFT Token ID"
+            className={`${styles.form__field} u__w100`}
+          />
+          {/* <ErrorMessage name="itemName" /> */}
+
+          <div>&nbsp;</div>
+          {/* <ErrorMessage name="itemDesc" /> */}
+        </div>
+        <div className="u__w100 u__center">
+          <button type="submit" className="btn gradient__green">
+            Submit
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
 };
 
-export default OuttakeForm;
+export default WithdrawForm;
